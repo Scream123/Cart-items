238e81b4a595092eae724fab691d6fb998cd8afd
Volodymyr Marakhovskyi <fly_life@mail.ru>
2025-02-21 02:26:23 +0200

    added  models

---
+++ app/Models/Cart.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Cart extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['user_id', 'user_session_id', 'product_id', 'quantity'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
