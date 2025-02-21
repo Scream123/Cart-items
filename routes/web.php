f98e61be5e06804df6efcfc3c410af73a7feb573
Volodymyr Marakhovskyi <fly_life@mail.ru>
2025-02-21 02:27:00 +0200

    added  routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/products', [ProductsController::class, 'index'])->name('products.index');
+Route::post('/cart/add/{id}', [ProductsController::class, 'addToCart'])->name('products.add');
+Route::post('/cart/update/{id}', [CartController::class, 'update'])->name('cart.update');
+Route::delete('/cart/remove/{id}', [CartController::class, 'removeCart'])->name('cart.remove');
+Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+
+
+
+
